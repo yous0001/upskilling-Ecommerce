605bb09c72777e8c98706a467620c4ff1a30090d
Yousef emad mikhail <mad33833@gmail.com>
2025-06-25 23:12:48 +0300

    add:get products & product by id

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -35,3 +35,23 @@ export const addProduct=catchAsync(async(req,res,next)=>{
     })
 })
 
+export const getProducts=catchAsync(async(req,res,next)=>{
+    const products=await Product.find()
+    res.status(200).json({
+        success:true,
+        products
+    })
+})
+
+export const getProductById=catchAsync(async(req,res,next)=>{
+    const {id}=req.params
+    const product=await Product.findById(id)
+
+    if(!product) 
+        return next(new AppError('Product not found',404))
+    
+    res.status(200).json({
+        success:true,
+        product
+    })
+})
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -6,3 +6,5 @@ import { auth } from "../middlewares/auth.middleware.js";
 export const productRouter=Router()
 
 productRouter.post('/',auth,productController.addProduct);
+productRouter.get('/',auth,productController.getProducts);
+productRouter.get('/:id',auth,productController.getProductById);
